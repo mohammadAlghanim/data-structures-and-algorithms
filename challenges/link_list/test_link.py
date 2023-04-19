f5856ee45c83607583813e0c9956fb9d0135810e
mohammadAlghanim <ghanimmohammad11@gmail.com>
2023-04-19 08:44:32 +0000

    challenge7

--- challenges/link_list/test_link.py
+++ challenges/link_list/test_link.py
@@ -1,6 +1,7 @@
 import pytest
 from link_list.challenge5.linklist.linked_list import Linklist
 from link_list.challenge6.linklist.linked_list import Linklist
+from link_list.challenge7.linklist.linked_list import Linklist
 
 def test_list1():
   ll = Linklist()
@@ -61,8 +62,26 @@ def test_8():
    actual = str(ll)
    assert actual == excepted
 
-   
-  
+def test_13(ll):
+    expected= "Error : Your input can't be more than the length" 
+    actual = ll.kthFromEnd(4)
+    assert  expected == actual  
+def test_14(ll):
+    expected= "Error : Your input can't be more than the length" 
+    actual = ll.kthFromEnd(7)
+    assert  expected == actual  
+
+def test_15():
+    LL= Linklist()
+    LL.insert("A")
+    assert LL.kthFromEnd(0) == "A"
+
+def test_16(ll):
+    expected= "B" 
+    actual = ll.kthFromEnd(2)
+    assert  expected == actual 
+
+ 
 
 
 
@@ -76,3 +95,12 @@ def ll():
     ll.insert("K")
     ll.insert("B")
     return ll
+@pytest.fixture(autouse=True)
+def clean():
+    """runs before each test automatically.
+    This is necessary because otherwise band instances added in one test
+    will bleed over to other tests
+    There's also a more advanced way to run code after each test as well
+    Check the docs for that. Hint: it uses yield
+    """
+    Linklist.count = 0
